c40fae279bc77d9afcd786031ec4b78e47db0efb
Hansem Ro <ro.edgar98@gmail.com>
2019-05-23 00:58:03 -0700

    Initial commit

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# Terminate existing processes
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar example
+
+echo "polybar launched"
+
