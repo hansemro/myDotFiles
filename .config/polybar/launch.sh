2c98ed23df39d3f61e52b717a111e986d0e800f1
Hansem Ro <ro.edgar98@gmail.com>
2019-09-22 10:31:02 -0700

    Add monitor fix. Change i3's resize bindings to match vim's.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-
 # Terminate existing processes
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar example
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
 
 echo "polybar launched"
 
